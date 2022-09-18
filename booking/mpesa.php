79838069a32bcf7f66f7538fe3cda9a499c1863d
DinanDias <93309200+DinanDias@users.noreply.github.com>
2022-09-18 21:17:07 +0530

    thank message after reservation

---
+++ booking/mpesa.php
@@ -0,0 +1,7 @@
+<td valign="top" class="body" style="padding-bottom:10px;">
+  <div id="mpesa">THANK YOU FOR MAKING YOUR RESRVATION .
+    TO PAY YOUR BILL VIA MPESA USE THIS NUMBER 
+    0714812921. YOU ARE EXPECTED TO PAY <?PHP echo $_SESSION['pay']; ?>
+	FOR THE MOST RECENT TRANSCTION YOU MADE.
+  </div>
+<td>
