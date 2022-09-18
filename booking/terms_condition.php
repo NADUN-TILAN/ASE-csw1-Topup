0db96a78a91f20eef628accee1edb798318bdb68
DinanDias <93309200+DinanDias@users.noreply.github.com>
2022-09-18 21:24:12 +0530

    terms and conditions display

---
+++ booking/terms_condition.php
@@ -0,0 +1,72 @@
+   <!--Modal Log-out --> 
+
+            <div class="modal fade" id="logout" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+
+<body>
+<div class="view">
+  <p>
+TERMS & CONDITIONS
+<ul>
+<h4>Reservations</h4>
+Through our online booking system, you will be able to check room availability (for most hotels) and make your booking. For peace of mind, you will receive by email a reservation confirmation, including the details of your reservation. It is recommended that you keep a copy of this reservation confirmation, preferably by printing the page.
+ 
+In the event that you have to modify or cancel your booking, please contact our reservation office as specified in your reservation confirmation.
+ 
+<h4> Guaranteed Reservations</h4>
+By securing your onlinebooking with a visa card, your booking  is guaranteed. All reservations made through our website must be guaranteed by a major credit card (e.g. Visa,, or direct palawan pawnshop).
+
+ 
+
+<h4>Cancellation </h4>
+<li>If there is a cancellation of a room reservation within 24hours prior to the arrival date, but the payments cannot refund once you already booking for the reservation.
+<li>Additional Requests
+<li>All additional or special requests are subject to availability and we cannot guarantee the provision for special requests. Any additional requests made should be prior to your arrival at the hotel, giving reasonable advance notice.
+<li>General Information
+<li>Accommodation: As a minimum, all bedrooms feature a private ensuite bathroom with either a bath & shower or a shower and colour television.  
+<li>Opening hours: Our reception is open 24hours. If you wish to check-in to the hotel out of these hours, please discuss with reception at the time of booking.
+<li>Payment Methods: We accept most major credit and debit cards, cash.
+
+<h4>Price Guarantee</h4>
+<li>On receipt of written confirmation the prices quoted and confirmed in writing by the Hotel remain fixed except for any alterations in the Government rates of taxation and/or duty such as VAT, for which we reserve the right to alter pricing to take account of any variation.
+<li>Making a booking
+<li>By making a booking you are confirming that you are authorised to do so on behalf of all persons named in the booking and you are acknowledging that all members of your party agree to be bound by these Booking Terms & Conditions.
+<li>When your booking has been made a confirmation can be sent to you by email using the email address that you have supplied, or by post. Booking confirmations are subject to the availability of accommodation at the hotel.
+<li>You should carefully check the details of your confirmation as soon as you receive it. You must contact Magbanua’s beach resort immediately if any of the details are incorrect or incomplete.
+<li>We will always endeavour to rectify any inaccuracies or accommodate any alterations you wish to make to your booking. We cannot accept liability for any inaccuracies that are not brought to our attention within seven days of issuing your confirmation, nor can we accept responsibility for inaccurate information that you have supplied.
+
+<h4>Paying for your booking</h4>
+Credit or debit card details will be required when you make your booking. No money will actually be taken although your card will be verified at the time of booking for 100% of your first nights stay. See our cancellation policy for charges that may be taken. In some instances, a non-refundable deposit or full payment may be required at the time of booking.
+Unless stated as part of your booking, additional items such as (but not limited to) the cost of external telephone calls, are not included in the price of your stay. If you incur any such additional costs you must settle the sum involved on your departure from the hotel.
+
+<h4>Circumstances beyond our control</h4>
+<li>We cannot accept responsibility for unforeseen circumstances beyond our control. These include (but are not limited to) adverse weather conditions, fire, riot, war, terrorist activity (or threat of such activity), industrial dispute, natural disaster, or injuries and death of an individual(s) through accidental circumstances unconnected with the hotel.
+<li>By making a booking you are accepting responsibility for any damage or loss caused by yourself or a member of your party. Full payment for any such damage or loss must be paid to the hotel owner or manager on demand. If you fail to do so, you will be responsible for meeting any claims subsequently made (together with our own and the other party's full legal costs) as a result of your actions.
+
+<h4>Complaints</h4>
+<li>If you are dissatisfied with any aspect of your stay you should bring the problem or issue to the attention of the duty manager or senior member of staff at the hotel as soon as possible so that all reasonable efforts can be made to rectify the situation. If, for any reason, the issue cannot be resolved to your satisfaction and you wish to make a complaint, you should put it in writing and send it to the owner  at: Dragon House , omiles cauayan negros occidental,.
+
+
+ If you have any questions, please email at magbanua.com or call (034) 4713 – 135 
+
+Thank you for choosing Dragon House
+
+Respectfully your,
+
+Dragon House
+</ul> 
+</p>
+</div>
+</body>
+</html>
+
+</div>
+ 
+</div>
+</div>      
+                                      
+          <!--Logout end -->  
